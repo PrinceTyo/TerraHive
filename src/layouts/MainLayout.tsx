82fb0c19b47682012c8bfae086865cafb90d0b16
PrinceTyo <kresnaale2@gmail.com>
2026-05-07 09:46:30 +0700

    first commit

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function MainLayout({ children }: Props) {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1 className="text-red-500 text-5xl">Hallo TerraHive</h1>
+    </section>
+  );
+}

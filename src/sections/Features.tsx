fb7d049fa86dc1def6e4ce0d3e8e9d1b24821ff0
PrinceTyo <kresnaale2@gmail.com>
2026-05-18 20:02:37 +0700

    fix: ui features-section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -12,24 +12,24 @@ export default function Features() {
           property services.
         </p>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-7 px-4 md:px-20 py-10">
-        <div className="bg-gray-100 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-200 shadow-lg hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
-          <IoLocationOutline className="w-12 h-12" />
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 px-4 md:px-20 py-10">
+        <div className="bg-gray-50 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-50 shadow-md hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
+          <IoLocationOutline className="w-16 h-16 p-3 rounded-lg bg-primary text-white" />
           <h2 className="text-2xl font-bold">Location</h2>
           <p>Find properties in strategic and highly desirable locations.</p>
         </div>
-        <div className="bg-gray-100 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-200 shadow-lg hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
-          <IoHomeOutline className="w-12 h-12" />
+        <div className="bg-gray-50 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-50 shadow-md hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
+          <IoHomeOutline className="w-16 h-16 p-3 rounded-lg bg-primary text-white" />
           <h2 className="text-2xl font-bold">Property Type</h2>
           <p>Explore modern homes, apartments, and luxury residences.</p>
         </div>
-        <div className="bg-gray-100 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-200 shadow-lg hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
-          <FaRegMoneyBillAlt className="w-12 h-12" />
+        <div className="bg-gray-50 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-50 shadow-md hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
+          <FaRegMoneyBillAlt className="w-16 h-16 p-3 rounded-lg bg-primary text-white" />
           <h2 className="text-2xl font-bold">Budget</h2>
           <p>Choose properties that match your budget and lifestyle.</p>
         </div>
-        <div className="bg-gray-100 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-200 shadow-lg hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
-          <MdOutlineRealEstateAgent className="w-12 h-12" />
+        <div className="bg-gray-50 p-8 rounded-xl text-center flex flex-col items-center justify-center gap-y-4 border-3 border-gray-50 shadow-md hover:text-white hover:bg-primary over:border-3 hover:border-primary hover:-translate-y-3 hover:shadow-2xl transition-all duration-300">
+          <MdOutlineRealEstateAgent className="w-16 h-16 p-3 rounded-lg bg-primary text-white" />
           <h2 className="text-2xl font-bold">Agent Bio</h2>
           <p>Connect with experienced agents ready to guide your journey.</p>
         </div>
